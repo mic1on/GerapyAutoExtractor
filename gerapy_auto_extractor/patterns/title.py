cfdabdd045737b2d8b9808f384378fe7368e0f54
miclon <jcnd@163.com>
2024-09-04 14:36:42 +0800

    ✨ feat: add article title metadata

--- gerapy_auto_extractor/patterns/title.py
+++ gerapy_auto_extractor/patterns/title.py
@@ -4,6 +4,8 @@ METAS = [
     '//meta[starts-with(@property, "title")]/@content',
     '//meta[starts-with(@name, "title")]/@content',
     '//meta[starts-with(@property, "page:title")]/@content',
+    '//meta[starts-with(@name, "ArticleTitle")]/@content',
+    '//meta[starts-with(@name, "article_title")]/@content',
 ]
 
 TITLE_HTAG_XPATH = '//h1//text() | //h2//text() | //h3//text() | //h4//text()'
